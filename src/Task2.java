5091d9c7394d16534be18a8ce234b254343fb168
hrachharutyunyan <43394235+hrachharutyunyan@users.noreply.github.com>
2019-10-31 15:42:30 +0400

    Task2

---
+++ src/Task2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("A:");
+        double a = scanner.nextDouble();
+        System.out.print("N:");
+        int n = scanner.nextInt();
+        double a2 = 1;
+        double result = 1;
+        for (int i = 2; i <= n; ++i) {
+            a2 *= -a;
+            result += a2;
+        }
+        System.out.println(result);
+    }
+}
+
