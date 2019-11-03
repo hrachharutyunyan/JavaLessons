546ad124c0608d98a29522f54749904289c3cdde
hrachharutyunyan <43394235+hrachharutyunyan@users.noreply.github.com>
2019-11-03 23:03:17 +0400

    Half of Methods

---
+++ src/Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        System.out.println(sumNum(a, b));
+    }
+
+    static int sumNum(int a, int b) {
+        return a + b;
+    }
+}
