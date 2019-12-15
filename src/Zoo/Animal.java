5ff496f2d5f10c3e707e901c782fcd0302eddb64
hrachharutyunyan <hrach.harutyunyan96@gmail.com>
2019-12-15 10:01:35 -0800

    Change class names

---
+++ src/Zoo/Animal.java
@@ -0,0 +1,32 @@
+package Zoo;
+
+class Animal {
+    private String name;
+    private String breed;
+    private int age;
+    private int weight;
+
+    Animal(String name, String breed, int age, int weight) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    String getAnimalInfo() {
+        return "имя: " + name + ", порода: " + breed + ", возраст: " + age + "лет" + ", вес: " + weight + "кг" + ".";
+    }
+
+   static String run(String direction) {
+        return " бегает " + direction;
+    }
+
+    static String voice(String voice) {
+        return " " + voice;
+    }
+
+    static String eat(String food) {
+        return " ест " + food;
+    }
+}
+
---
+++ src/Zoo/Main.java
@@ -0,0 +1,8 @@
+package Zoo;
+
+public class Main {
+    public static void main(String[] args) {
+        Visitor Sona =new Visitor("Sona","Aleqsanyan","Armenian",2);
+        System.out.println(Sona.getVisitorInfo());
+    }
+}
