b52656701c7ac3f9547bbb6ff67d024b88169a26
hrachharutyunyan <hrach.harutyunyan96@gmail.com>
2019-12-25 11:18:07 -0800

    fix

--- src/TaskFive.java
+++ src/TaskFive.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 public class TaskFive {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
+        int[] array = {1, 2, 3, 4, 5}; 
         System.out.println(Arrays.toString(reverseArray(array)));
     }
-    static int[] reverseArray(int array[]) {
+    static int[] reverseArray(int[] array) {
 
         for (int i = 0; i < array.length / 2; i++) {
             var temp = array[i];
