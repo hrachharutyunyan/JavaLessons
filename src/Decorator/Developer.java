a3dc13bc5dac2d1fbeada70a5468c3b9953a687d
hrachharutyunyan <hrach.harutyunyan96@gmail.com>
2020-01-19 08:48:58 -0800

    Decorator pattern

---
+++ src/Decorator/Developer.java
@@ -0,0 +1,6 @@
+package Decorator;
+
+public interface Developer {
+
+    String makeJob();
+}
---
+++ src/Decorator/JavaTeamLead.java
@@ -0,0 +1,16 @@
+package Decorator;
+
+public class JavaTeamLead extends DeveloperDecorator {
+
+    public JavaTeamLead(Developer developer) {
+        super(developer);
+    }
+
+    public String sendWeekReport() {
+        return "Send week report to customers.";
+    }
+
+    public String makeJob() {
+        return super.makeJob() + " " + sendWeekReport();
+    }
+}
