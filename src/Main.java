4e9db0cc839ce915f8e9ad201266ea8a09369db2
hrachharutyunyan <hrach.harutyunyan96@gmail.com>
2020-01-24 12:31:25 -0800

    Singleton

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Singleton x = Singleton.getInstance();
         Singleton y = Singleton.getInstance();
         Singleton z = Singleton.getInstance();
