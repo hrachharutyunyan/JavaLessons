d27b23b65b896c39f3f9233ce177b6a638b90e63
hrachharutyunyan <hrach.harutyunyan96@gmail.com>
2020-01-25 02:21:12 -0800

    Command

---
+++ src/HomeAutomationDemo.java
@@ -0,0 +1,17 @@
+public class HomeAutomationDemo {
+
+    public static void main(String[] args) {
+
+        Controller controller = new Controller();
+        Light light = new Light();
+
+        Command lightsOn = new LightOnCommand(light);
+        Command lightsOff = new LightOffCommand(light);
+
+        controller.setCommand(lightsOn);
+        controller.executeCommand();
+
+        controller.setCommand(lightsOff);
+        controller.executeCommand();
+    }
+}
