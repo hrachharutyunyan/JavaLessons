93d43bf8cf1f69217775a57d03dbdde211b0a518
hrachharutyunyan <hrach.harutyunyan96@gmail.com>
2020-02-01 11:50:17 -0800

    Decorator pattern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Test exception");
+        try {
+            System.out.println("test 1");
+            System.out.println(5 / 0);
+            System.out.println("test 2");
+        } catch (Exception e) {
+            System.out.println("Number must not be zero");
+        }
+        System.out.println("Finish");
     }
 }
