c8f98575a5400e228d7ed75ef8aff75f5c5b8266
hrachharutyunyan <hrach.harutyunyan96@gmail.com>
2020-02-01 11:51:23 -0800

    Exception

--- src/Exception.java
+++ src/Exception.java
@@ -1,11 +1,11 @@
-public class Main {
+public class Exception {
     public static void main(String[] args) {
         System.out.println("Test exception");
         try {
             System.out.println("test 1");
             System.out.println(5 / 0);
             System.out.println("test 2");
-        } catch (Exception e) {
+        } catch (java.lang.Exception e) {
             System.out.println("Number must not be zero");
         }
         System.out.println("Finish");
